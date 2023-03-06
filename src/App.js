32c2e12a25e8d76ed96c65a6906b15aaa308df00
Simon Cordova <simon.cordova@creative-cx.com>
2023-03-06 09:28:44 +0000

    add total per person and other changes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [baseAmount, setBaseAmount] = useState(0);
-  const [tipPercentage, setTipPercentage] = useState(0);
-  const [tipValue, setTipValue] = useState(0);
+  const [baseAmount, setBaseAmount] = useState(100);
+  const [tipPercentage, setTipPercentage] = useState(15);
+  const [totalTip, setTotalTip] = useState(0);
   const [totalAmount, setTotalAmount] = useState(0);
-  const [people, setPeople] = useState(2);
+  const [people, setPeople] = useState(1);
   const [tipPerPerson, setTipPerPerson] = useState(0);
-  // const [totalPerPerson, setTotalPerPerson] = useState(0);
+  const [totalPerPerson, setTotalPerPerson] = useState(0);
 
   const handleBaseAmount = (event) => {
-    const value = parseInt(event.target.value);
+    const value = parseFloat(event.target.value);
 
     if (value === '' || isNaN(value)) {
       setBaseAmount(0)
@@ -31,8 +31,17 @@ function App() {
     setPeople(value);
   }
 
+  const calculateTotalPerPerson = () => {
+    const totalPerPerson = (totalAmount / people).toFixed(2);
+    if (isFinite(totalPerPerson) === false || isNaN(totalPerPerson)) {
+      setTotalPerPerson(0);
+    } else {
+      setTotalPerPerson(totalPerPerson);
+    }
+  }
+
   const calculateTipPerPerson = () => {
-    const tipPerPerson = (tipValue / people).toFixed(2);
+    const tipPerPerson = (totalTip / people).toFixed(2);
     if (tipPerPerson === 'Infinity' || isNaN(tipPerPerson)) {
       setTipPerPerson(0);
     } else {
@@ -43,7 +52,7 @@ function App() {
   const calculateTotalAmount = () => {
     const tipAsDecimal = tipPercentage / 100;
     const tip = parseFloat((baseAmount * tipAsDecimal).toFixed(2));
-    setTipValue(tip);
+    setTotalTip(tip);
 
     const totalAmount = (baseAmount + tip).toFixed(2);
     setTotalAmount(totalAmount);
@@ -52,11 +61,13 @@ function App() {
   useEffect(() => {
     calculateTotalAmount();
     calculateTipPerPerson();
+    calculateTotalPerPerson();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [baseAmount, tipPercentage]);
 
   useEffect(() => {
     calculateTipPerPerson();
+    calculateTotalPerPerson();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [people, totalAmount])
 
@@ -95,7 +106,7 @@ function App() {
             <input
               type="range"
               min="0"
-              max="10"
+              max="20"
               defaultValue={people}
               className="home__tip-calculator__slider-meter"
               onInput={handlePersonSlider}
@@ -109,8 +120,8 @@ function App() {
         <hr />
         <div className="home__tip-calculator-results">
           <div className="category home__tip-calculator__results-tip-amount">
-            <p>Tip Value: </p>
-            <p>${tipValue}</p>
+            <p>Total Tip: </p>
+            <p>${totalTip}</p>
           </div>
           <div className="category home__tip-calculator__results-tip-amount">
             <p>Tip per person:</p>
@@ -119,6 +130,10 @@ function App() {
           <div className="category home__tip-calculator__results-total-bill-with-tip">
             <p>Total Amount:</p>
             <p>${totalAmount}</p>
+          </div>
+          <div className="category home__tip-calculator__results-total-bill-per-person">
+            <p>Total per person:</p>
+            <p>${totalPerPerson}</p>
           </div>
         </div>
       </div>
